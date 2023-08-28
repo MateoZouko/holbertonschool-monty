f36a8233a6ec41d7903fad7cc601342fb6c9cddc
seba17962 <isebsebastiansilva@gmail.com>
2023-08-28 21:30:39 +0000

    add the prototype of push and pall functions

--- monty.h
+++ monty.h
@@ -35,4 +35,7 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-#endif
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+
+#endif /** MONTY_H */
