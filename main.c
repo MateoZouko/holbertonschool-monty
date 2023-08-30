89f0de000e69cc26459e5535f5942ada4070ceb7
MateoZouko <mateozuko1@gmail.com>
2023-08-30 15:07:20 +0000

    fixing

--- main.c
+++ main.c
@@ -2,60 +2,63 @@
 
 int main(int argc, char *argv[])
 {
-    stack_t *stack = NULL;
-    char line[100];
-    unsigned int line_number = 0;
-    int value;
-    char *opcode;
-    char *arg;
+	stack_t *stack = NULL;
+	char *line = NULL;
+	size_t line_size = 0;
+	unsigned int line_number = 1;
+	int value;
+	char *opcode;
+	char *arg;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        return (EXIT_FAILURE);
-    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: %s file\n", argv[0]);
+		return (EXIT_FAILURE);
+	}
 
-    FILE *file = fopen(argv[1], "r");
-    if (file == NULL)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        return (EXIT_FAILURE);
-    }
+	FILE *file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		perror("Error: Can't open file");
+		return (EXIT_FAILURE);
+	}
 
-    while (fgets(line, sizeof(line), file))
-    {
-        line[strcspn(line, "\n")] = '\0';
+	while (getline(&line, &line_size, file) != -1)
+	{
+		line[strcspn(line, "\n")] = '\0';
 
-        opcode = strtok(line, " ");
-        if (opcode != NULL)
-        {
-            if (strcmp(opcode, "push") == 0)
-            {
-                arg = strtok(NULL, " ");
-                if (arg == NULL)
-                {
-                    printf("L%u: usage: push integer\n", line_number);
-                    continue;
-                }
+		opcode = strtok(line, " ");
+		if (opcode != NULL)
+		{
+			if (strcmp(opcode, "push") == 0)
+			{
+				arg = strtok(NULL, " ");
+				if (arg == NULL)
+				{
+					printf("L%u: usage: push integer\n", line_number);
+					continue;
+				}
 
-                value = atoi(arg);
-                if (value == 0 && strcmp(arg, "0") != 0)
-                {
-                    printf("L%u: usage: push integer\n", line_number);
-                    continue;
-                }
+				value = atoi(arg);
+				if (value == 0 && strcmp(arg, "0") != 0)
+				{
+					printf("L%u: usage: push integer\n", line_number);
+					continue;
+				}
 
-                push(&stack, line_number);
-            }
-            else if (strcmp(opcode, "pall") == 0)
-            {
-                pall(&stack, line_number);
-            }
-        }
+				push(&stack, line_number);
+			}
+			else if (strcmp(opcode, "pall") == 0)
+			{
+				pall(&stack, line_number);
+			}
+		}
 
-        line_number++;
-    }
+		line_number++;
+	}
 
-    fclose(file);
-    return (EXIT_SUCCESS);
+	fclose(file);
+	if (line)
+		free(line);
+	return (EXIT_SUCCESS);
 }
