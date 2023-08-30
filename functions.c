948672f6a2974db85787d65fa46d4ee9b8201cc0
MateoZouko <mateozuko1@gmail.com>
2023-08-30 16:29:27 +0000

    fixing functions

--- functions.c
+++ functions.c
@@ -21,7 +21,7 @@ void _push(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-	if (!token || _digit(token) == 0 || !stack)
+	if (!token || isdigit(token) == 0 || !stack)
 	{
 		fprintf(stderr, "L%u: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
