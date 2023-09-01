498bc607b1dcf09968b487eaf193cdad2be5a088
MateoZouko <mateozuko1@gmail.com>
2023-09-01 14:10:31 +0000

    fixing swap func

--- functions.c
+++ functions.c
@@ -75,7 +75,7 @@ void _swap(stack_t **stack, unsigned int line_number)
 
 	if (!(*stack) || !(*stack)->next)
 	{
-		fprintf(stderr, "L%d can't swap, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	x = (*stack)->n;
