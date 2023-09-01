eb25bb28ecaa47b59f8cd74c3d07493bf45bdd5a
seba17962 <isebsebastiansilva@gmail.com>
2023-09-01 14:48:08 +0000

    fix betty

--- functions.c
+++ functions.c
@@ -98,9 +98,9 @@ void _pop(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	
+
 		*stack = (*stack)->next;
-	
+
 		free(tmp);
 }
 
